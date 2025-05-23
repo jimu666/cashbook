98cfb926401c91f806be4f82dd3537dd52491b01
jimu666 <84886866+jimu666@users.noreply.github.com>
2025-05-23 23:23:27 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ echo "============================================="
 # 打印环境信息
 # echo "Checking and creating database if it does not exist..."
 echo "Starting application with Prisma database initialization..."
-npx prisma migrate deploy
+#npx prisma migrate deploy
 echo "Success Run npx prisma migrate deploy."
 
 # 启动应用程序
