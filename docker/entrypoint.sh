92c28f1bc9e6703b3e42c06135f5c2875a85b51e
jimu666 <84886866+jimu666@users.noreply.github.com>
2025-05-23 23:51:49 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
-echo "============================================="
-echo "欢迎使用 Cashbook"
-echo "============================================="
-# 打印环境信息
-# echo "Checking and creating database if it does not exist..."
-echo "Starting application with Prisma database initialization..."
-#npx prisma migrate deploy
-echo "Success Run npx prisma migrate deploy."
 
-# 启动应用程序
+# 强制指定使用本地引擎
+export PRISMA_QUERY_ENGINE_LIBRARY=/app/prisma-engines/libquery_engine.so.node
+export PRISMA_QUERY_ENGINE_BINARY=/app/prisma-engines/query-engine
+export PRISMA_SCHEMA_ENGINE_BINARY=/app/prisma-engines/schema-engine
+export PRISMA_FMT_BINARY=/app/prisma-engines/prisma-fmt
+
+echo "Prisma binary paths set."
+echo "Running Prisma database migration..."
+
+# 使用本地引擎 + 指定版本的 CLI 进行迁移
+npx prisma@5.1.4 migrate deploy
+
+echo "Migration completed."
 echo "Starting application..."
+
 exec node server/index.mjs
