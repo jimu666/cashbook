ee7109852ce13f551fe8ec38b5a97739130140cf
jimu666 <84886866+jimu666@users.noreply.github.com>
2025-05-24 10:51:05 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,8 +11,8 @@ export PRISMA_ENGINES_CHECKSUM_IGNORE_MISSING=1
 
 echo "Prisma binary paths set."
 
-# 运行 Prisma 迁移（使用已安装的本地 CLI，不使用 npx）
-node_modules/.bin/prisma migrate deploy
+# 运行 Prisma 迁移（使用已安装的本地 CLI，不使用 npx）无迁移引擎
+#node_modules/.bin/prisma migrate deploy
 
 echo "Migration completed."
 echo "Starting application..."
